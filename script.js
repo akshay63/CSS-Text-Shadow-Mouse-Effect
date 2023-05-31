21af15584e53509d3f71e213d67033b7083289ff
Akshaye <meshakshay9860u@gmail.com>
2023-05-31 22:46:44 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+ **--------- Project16: CSS Text Shadow Mouse Effect ---------**
+ */
+
+//WORKING
+//1) We will listen to "mouseover" event on 'hero' ele
+//2) When the event happens we're going to figure out how far and where should we put the text shadow on h1
+
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 50;
+
+function handleMouse(e) {
+  //Learning: Destructuring
+  // const width = hero.offsetWidth;
+  // const height = hero.offsetHeight;
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
+
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 gray`;
+  //   text.style.textShadow = `
+  //   ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+  //   ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+  //   ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+  //   ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+  // `;
+}
+
+hero.addEventListener("mousemove", handleMouse);
